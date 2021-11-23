c1db444165c820d7620194b4ba2672f923d9c212
baruasgrps <baruasgrps@gmail.com>
2021-11-24 00:14:14 +0600

    New commit

---
+++ ManageRoles/Controllers/PasswordController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManageRoles.Algorithm;
+
+namespace ManageRoles.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // GET: Password
+        public ActionResult Index()
+        {
+            AesAlgorithm aesAlgorithm = new AesAlgorithm();
+            var storedpassword = aesAlgorithm.DecryptString("07ikhQIubF3dXMDo3glk5A==");
+            Console.WriteLine("Password: " + storedpassword);
+            return View();
+        }
+    }
+}
